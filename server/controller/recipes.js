addf32ba297b557300c9d5f7c7846e7f825231af
samuel <difrentecho@gmail.com>
2017-09-27 20:43:13 +0100

    Creates add recpe controller

--- server/controller/recipes.js
+++ server/controller/recipes.js
@@ -3,37 +3,42 @@ import Recipes from '../models/recipes';
 class Recipe {
   
   static ingredient(req, res){
-
-    let l = recipes.ingredients.lenght;
-    const id = Recipes.ingredient[l-1];
+    const { ingredient, recipId } = req.body;
+    let l = Recipes.ingredients.lenght;
+    const id = 1 + l;
 
     Recipes.ingredients.push({
       id: id,
       recipId: recipId,
-      ingredient: ingredients
-    })     
-    return res.ststus(200).send(Recipes.ingredients)
+      ingredient: ingredient
+    });     
+    return res.ststus(200).send(Recipes.ingredients);
   }
 
   static add(req, res) {
-    const { recipeName, mealType, dishType, method } = req.body;
+    const { recipeName, mealType, dishType, method, ingredients } = req.body;
     if (!recipeName) {
       res.status(400).send({
         message: 'Please Enter Recipe Name'
-      })
+      });
     } else if (!mealType) {
-       res.status(400).send({
+      res.status(400).send({
         message: 'Please Enter Meal Type'
-      })
+      });
     } else if (!dishType) {
       res.status(400).send({
         message: 'Please Enter Dish Type'
-      })
+      });
     } else if (!method) {
-       res.status(400).send({
+      res.status(400).send({
         message: 'Please Enter Method'
-      })
-    } else {
+      });
+    } 
+    else if (!ingredients) {
+      res.status(400).send({
+        message: 'Please Enter Ingredients'
+      });
+    }else {
 
       let l = Recipes.length;
       const id = 1 + l;
@@ -44,17 +49,48 @@ class Recipe {
         recipeName: recipeName,
         mealType: mealType,
         dishType: dishType,
-        method: method
-      })
+        method: method,
+        ingredients: { ingredients }
+      });
 
-      res.status(200).send(Recipes)
+      res.status(200).send(Recipes);
     }
     
     //return res.status(200).send(recipes.ingredients)
   }
   
+  static update(req, res) {
+    const updateFields = {};
+    const  id = req.params.Id;
+    const { recipeName, mealType, dishType, method, ingredients } = req.body;
+    const found = false;
+    console.log(id);
+    for (let i = 0; i < Recipes.length; i++) {
+      if (Recipes[i].id === parseInt(id, 10)){
+        if (recipeName) {
+          updateFields.recipeName = recipeName;
+        }else if (mealType) {
+          updateFields.mealType = mealType;
+        } else if (dishType) {
+          updateFields.method = method;
+        } else if (ingredients) {
+          updateFields.ingredients = ingredients;
+        } else {
+          return res.status(200).send({
+            Message: 'Nothing to update'
+          });
+        }
+      } else {
+        return res.status(404).send({
+          message: 'Recipe Not found!'
+        });
+      }
+    }
+
+  }
+
   static test(req, res) {
-    return res.status(200).send(Recipes.ingredients)
+    return res.status(200).send(Recipes.ingredients);
   }
 
   
--- server/models/recipes.js
+++ server/models/recipes.js
@@ -7,11 +7,7 @@ recipes = [
     recipeName: 'Rice and Beans',
     mealType: 'Lunch',
     dishType: 'Nigerian Delicay',
-    method: 'this is just a dummy so i can write what ever i want in here ok? thanks you@',
-    ingredient: [
-      'rice',
-      'beans',
-    ]
+    method: 'this is just a dummy so i can write what ever i want in here ok? thanks you@'
   },
   {
     id: 2,
@@ -32,27 +28,8 @@ recipes = [
 
 ]
 
-recipes.ingredients = [
-  {
-    id: 1,
-    recipeId: 1,
-    ingredient: '2cups of rice and 1cup of beans'
-  },
-  {
-    id: 2,
-    recipeId: 1,
-    ingredient: 'Five peices of Tomatoes'
-  },
-  {
-    id: 3,
-    recipeId: 1,
-    ingredient: '2 tea spoons of salt'
-  }
-
-  
-]
-
 /* let l = recipes.length;
 const id = 1 + l;
 console.log(id)  */
+
 export default recipes
