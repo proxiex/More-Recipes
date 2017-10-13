9e132e164f93411cb4ab1742cafe133eb57698b7
samuel <difrentecho@gmail.com>
2017-10-14 00:49:20 +0100

    Add signup form and its state

--- client/components/signup/signupPage.js
+++ client/components/signup/signupPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import SignupForm from './signupForm';
 
 class SignupPage extends React.Component {
   render () {
     return (
-      <div>
-        <h3>Signup page</h3>   
+      <div id="wrapper" className="contiainer center">
+        <div className="row">
+            <div className="col s12 m4 offset-m4">
+              <h4 className="white-text">Signup</h4>
+                <SignupForm />
+            </div>      
+        </div>
       </div>
     );
   }
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -19,6 +19,13 @@ export default {
   ],
   module: {
     loaders: [
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      },
       { 
         test: /\.css$/, 
         loader: 'style-loader!css-loader'
