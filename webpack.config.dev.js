ba84345f8c4744ac994ed22f4665fc4c7020af73
samuel <difrentecho@gmail.com>
2017-10-14 01:38:33 +0100

    modify webpack.config.dev.js file

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,7 +1,7 @@
-import path from 'path';
-import webpack from 'webpack';
+const path = require('path');
+const webpack = require('webpack');
 
-export default {
+module.exports = {
   devtool: 'eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
