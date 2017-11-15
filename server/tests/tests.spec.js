e8dcf15ca4fe2a928caca0a75d3b8d983012f2bb
proxiex <difrentecho@gmail.com>
2017-11-15 21:02:26 +0100

    update test.spec.js

--- server/tests/tests.spec.js
+++ server/tests/tests.spec.js
@@ -12,8 +12,6 @@ let token;
 let id;
 let userId;
 
-improt 'users.text'
-
 describe('More Recipes', () => {
   it('should get the home page', (done) => {
     chai.request(app)
@@ -108,7 +106,8 @@ describe('Users Controller', () => {
       .send(fakeData.newUsers)
       .end((err, res) => {
         res.should.be.json;
-        userId = res.body.register.id;
+
+        userId = res.body.newUser.id;
         console.log(res.body)
         res.body.should.be.a('object');
         res.should.have.status(201);
@@ -284,7 +283,6 @@ describe('Favorite Recipes Controller', () => {
       .post('/api/v1/users/x3s/recipes')
       .set('x-token', token)
       .end((err, res) => {
-        console.log(err)
         res.should.have.status(400);
         res.should.be.json;
         res.body.should.have.property('message').equal('Parameter must be a number!');
